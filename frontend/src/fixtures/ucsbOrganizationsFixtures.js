e4f4c29c8ea5cf877a45c93c56e69ba562f8046b
lawrencewang30 <jiabin2003@gmail.com>
2023-08-03 23:13:14 -0700

    lw - implemented ucsbOrganizationsFixtures

---
+++ frontend/src/fixtures/ucsbOrganizationsFixtures.js
@@ -0,0 +1,32 @@
+const ucsbOrganizationsFixtures = {
+    oneOrganization: {
+        "orgCode": "TASA",
+        "orgTranslationShort": "Taiwanese American Student Association",
+        "orgTranslation": "Taiwanese American Student Association at UCSB",
+        "inactive": false
+    },
+
+    threeOrganizations: [
+        {
+            "orgCode": "ZPR",
+            "orgTranslationShort": "Zeta Phi Rho",
+            "orgTranslation": "Zeta Phi Rho",
+            "inactive": false
+        },
+        {
+            "orgCode": "SKY",
+            "orgTranslationShort": "Skydiving Club",
+            "orgTranslation": "Skydiving Club at UCSB",
+            "inactive": false
+        },
+        {
+            "orgCode": "UCSBMCB",
+            "orgTranslationShort": "Club Basketball",
+            "orgTranslation": "UCSB Men's Club Basketball",
+            "inactive": true
+        }
+
+    ]
+};
+
+export { ucsbOrganizationsFixtures};
