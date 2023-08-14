052308b3980dc6fefba70564984311d5f00f75a6
Danny You <kyou@ucsb.edu>
2023-08-14 15:26:16 -0700

    dy - implemented form component of Articles

--- frontend/src/main/components/UCSBDates/UCSBDateForm.js
+++ frontend/src/main/components/UCSBDates/UCSBDateForm.js
@@ -24,7 +24,7 @@ function UCSBDateForm({ initialContents, submitAction, buttonLabel = "Create" })
 
     // Stryker disable next-line all
     const yyyyq_regex = /((19)|(20))\d{2}[1-4]/i; // Accepts from 1900-2099 followed by 1-4.  Close enough.
-
+    
     return (
 
         <Form onSubmit={handleSubmit(submitAction)}>
